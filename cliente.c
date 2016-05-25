60aeb2a326564bbae1b6012cc66ae89f2ebc5e5b
BRUNO VINICIUS CASSOL ORENGO <16104011@l1801166.portoalegre.pucrsnet.br>
2016-05-25 11:26:43 -0300

    funciona pls

--- cliente.c
+++ cliente.c
@@ -2,15 +2,15 @@
 
 struct cliente {
 	char nome[41];	//até 40 caracteres
-	int idade;	//em anos
-	char endereço[100];	//até 100 caracteres
+	int anoNascimento;	//qutro digitos
+	char endereco[101];	//até 100 caracteres
 	int conta;	//número da conta no vetor de contas
-	int cgc;	//número no Cadastro Geral de Contribuintes (CGC)
+	char cgc[16];	//número no Cadastro Geral de Contribuintes (CGC)
 } clientes[2000];
 
 struct conta{
 	int cliente;	//número do cliente no vetor de contas
-	int tempo;	//em anos, desde a abertura da conta
+	int anoAbertura;	//quatro digitos
 	float saldo;	//em reais
 } contas[2000];
 
